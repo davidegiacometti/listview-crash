c0b997e0aa2bf256b4ed67770b1d75abef53f085
Davide <davide.giacometti@outlook.it>
2022-09-05 22:47:27 +0200

    sample

---
+++ Playground/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Playground
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<string> Collection { get; set; }
+
+        public MainViewModel()
+        {
+            Collection = new ObservableCollection<string>
+            {
+                "Cat",
+                "Dog",
+                "Wolf",
+                "Tiger",
+                "Dragon",
+            };
+        }
+    }
+}
